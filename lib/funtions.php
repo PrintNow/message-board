267c97b6e972d7096fb1de270f3035ea846ba1af
chuwen <chenwenzhou@aliyun.com>
2020-06-14 14:50:11 +0800

    添加 authocode 对称加密函数

--- lib/funtions.php
+++ lib/funtions.php
@@ -42,34 +42,36 @@ function getIp()
  * @param array $data
  * @return false|string
  */
-function echoApiData($code=0, $msg='ok', $data=[]){
+function echoApiData($code = 0, $msg = 'ok', $data = [])
+{
     $data = [
         'code' => $code,
         'msg' => $msg,
         'data' => $data
     ];
-    if(count($data) < 1) unset($data['data']);
+    if (count($data) < 1) unset($data['data']);
 
     return json_encode($data, JSON_UNESCAPED_UNICODE);
 }
 
 
-function formatTime($time){
+function formatTime($time)
+{
     //现在的 年 月 日
     list($y, $m, $d, $h_i) = explode("-", date("Y-m-d-H:i"));
 
     //传入时间的 年 月 日
     list($_y, $_m, $_d, $_h_i) = explode("-", date("Y-m-d-H:i", $time));
 
-    if($y > $_y){
+    if ($y > $_y) {
         $prefix = "{$y}-{$m}-{$d}";
     }
 
-    if($y === $_y){
+    if ($y === $_y) {
         $prefix = "{$m}-{$d}";
     }
 
-    if($d === $_d){
+    if ($d === $_d) {
         $prefix = '今天';
     }
 
@@ -112,18 +114,18 @@ function post($field = '', $default = false)
 
 
 /**
- * @param   int     $maxpage  总页数
- * @param   int     $page    当前页
- * @param   string  $para   翻页参数(不需要写$page),如http://www.example.com/article.php?page=3&id=1，$para参数就应该设为'&id=1'
+ * @param int $maxpage 总页数
+ * @param int $page 当前页
+ * @param string $para 翻页参数(不需要写$page),如http://www.example.com/article.php?page=3&id=1，$para参数就应该设为'&id=1'
  * @return  string          返回的输出分页html内容
  */
-function multipage($maxpage=5, $page=1, $para = '')
+function multipage($maxpage = 5, $page = 1, $para = '')
 {
     $multipage = '';  //输出的分页内容
     $listnum = 5;     //同时显示的最多可点击页面
 
     if ($maxpage < 2) {
-        return '<ul class="pagination"><li class="active"><a href="?page=1'.$para.'">1</a></li></ul>';
+        return '<ul class="pagination"><li class="active"><a href="?page=1' . $para . '">1</a></li></ul>';
     } else {
         $offset = 2;
         if ($maxpage <= $listnum) {
@@ -160,4 +162,76 @@ function multipage($maxpage=5, $page=1, $para = '')
     }
 
     return $multipage;
+}
+
+
+/**
+ * desc 加密和解密
+ * @param string $string 需要加密或解密的字符串
+ * @param string $operation DECODE:解密;ENCODE:加密;
+ * @param string $key 秘钥
+ * @param int $expiry 密文有效期
+ * @return bool|string
+ */
+function authcode($string, $operation = 'DECODE', $key = '', $expiry = 0)
+{
+    // 动态密匙长度，相同的明文会生成不同密文就是依靠动态密匙
+    $ckey_length = 4;
+
+    // 密匙
+    $key = md5($key ? $key : $GLOBALS['discuz_auth_key']);
+
+    // 密匙a会参与加解密
+    $keya = md5(substr($key, 0, 16));
+    // 密匙b会用来做数据完整性验证
+    $keyb = md5(substr($key, 16, 16));
+    // 密匙c用于变化生成的密文
+    $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length) :
+        substr(md5(microtime()), -$ckey_length)) : '';
+    // 参与运算的密匙
+    $cryptkey = $keya . md5($keya . $keyc);
+    $key_length = strlen($cryptkey);
+    // 明文，前10位用来保存时间戳，解密时验证数据有效性，10到26位用来保存$keyb(密匙b)，
+//解密时会通过这个密匙验证数据完整性
+    // 如果是解码的话，会从第$ckey_length位开始，因为密文前$ckey_length位保存 动态密匙，以保证解密正确
+    $string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) :
+        sprintf('%010d', $expiry ? $expiry + time() : 0) . substr(md5($string . $keyb), 0, 16) . $string;
+    $string_length = strlen($string);
+    $result = '';
+    $box = range(0, 255);
+    $rndkey = array();
+    // 产生密匙簿
+    for ($i = 0; $i <= 255; $i++) {
+        $rndkey[$i] = ord($cryptkey[$i % $key_length]);
+    }
+    // 用固定的算法，打乱密匙簿，增加随机性，好像很复杂，实际上对并不会增加密文的强度
+    for ($j = $i = 0; $i < 256; $i++) {
+        $j = ($j + $box[$i] + $rndkey[$i]) % 256;
+        $tmp = $box[$i];
+        $box[$i] = $box[$j];
+        $box[$j] = $tmp;
+    }
+    // 核心加解密部分
+    for ($a = $j = $i = 0; $i < $string_length; $i++) {
+        $a = ($a + 1) % 256;
+        $j = ($j + $box[$a]) % 256;
+        $tmp = $box[$a];
+        $box[$a] = $box[$j];
+        $box[$j] = $tmp;
+        // 从密匙簿得出密匙进行异或，再转成字符
+        $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+    }
+    if ($operation == 'DECODE') {
+        // 验证数据有效性，请看未加密明文的格式
+        if ((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) &&
+            substr($result, 10, 16) == substr(md5(substr($result, 26) . $keyb), 0, 16)) {
+            return substr($result, 26);
+        } else {
+            return '';
+        }
+    } else {
+        // 把动态密匙保存在密文里，这也是为什么同样的明文，生产不同密文后能解密的原因
+        // 因为加密后的密文可能是一些特殊字符，复制过程可能会丢失，所以用base64编码
+        return $keyc . str_replace('=', '', base64_encode($result));
+    }
 }
